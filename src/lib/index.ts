1b3f26c774386d3a7d6b1c2605eedd95724c3ee3
Seungbong Lee <mervonee@gmail.com>
2022-07-12 16:56:10 +0900

    :bookmark: Releasing / Version tags. - add ignore configuration - remove un used css include

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,3 @@
-// import 'react-sortable-tree/style.css';
-// import 'slick-carousel/slick/slick.css';
-import '../../old-style/Calendar/calendar-style.css';
-import '../../old-style/Slick/slick-style.css';
-import '../../old-style/Tree/node-renderer-default.css';
-
 export * from './decorator';
 export * from './model';
 export * from './ui';
